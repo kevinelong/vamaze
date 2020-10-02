cb80f19f866863c6c1a9a67c9489136bdf5ac498
kevinelong <kevinelong@gmail.com>
2020-10-02 13:55:01 -0700

    add monsters

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from board import Board
-from player import Player
+from player import Player, Monster
+import random
 
 
 class Game:
     def __init__(self):
         self.level = 1
+        self.moves = 0
         self.board = Board(9)
-        self.player = Player("A", "^", self.board.player_location)
+        self.player = Player("A", "^")
         self.playing = True
         self.exit_location = [0, 0]
         self.walls = []
+        self.monsters = []
         self.new_level()
 
     def new_level(self):
+        self.moves = 0
         self.board = Board(9)
         self.exit_location = self.board.place_random("X")
         self.walls = []
-        for w in range(self.board.size * self.level):
+        self.monsters = []
+        for m in range((self.board.size // 2) * self.level):
+            monster = Monster("M")
+            monster.location = self.board.place_random("M")
+            self.monsters.append(monster)
+        for w in range((self.board.size // 2) * self.level):
             self.walls.append(self.board.place_random("O"))
+        self.player.health = 100
 
     def loop(self):
         while self.playing:
@@ -34,7 +44,10 @@ class Game:
     def user_input(self):
         text = input("Which direction? or q to quit.")
         upper = text.upper()
-        self.player.old_location = self.player.location
+        self.player.old_location = [self.player.location[0], self.player.location[1]]
+
+        self.moves += 1
+
         if upper == "Q":
             self.playing = False
         elif upper in ["NORTH", "UP", "W"]:
@@ -61,8 +74,12 @@ class Game:
     def populate(self):
         self.board.clear()
 
+        for monster in self.monsters:
+            self.board.place(monster.location[0], monster.location[1], "M")
+
         for w in self.walls:
             self.board.place(w[0], w[1], "O")
+
         self.board.place(self.exit_location[0], self.exit_location[1], "X")
         self.board.place(self.player.location[0], self.player.location[1], self.player.symbol)
 
@@ -71,12 +88,39 @@ class Game:
         for w in self.walls:
             if self.player.location == w:
                 self.player.location = self.player.old_location
-                print("block")
+                self.player.location = [self.player.old_location[0], self.player.old_location[1]]
+
+                print("BLOCKED!")
+        for monster in self.monsters:
+            if self.player.location == monster.location:
+                print("FIGHT!")
+                self.fight(self.player, monster)
 
         if self.player.location == self.exit_location:
-            print("YOU WIN THIS LEVEL")
+            print(f"YOU WON THIS LEVEL IN {self.moves} MOVES!")
             self.level += 1
             self.new_level()
+
+    def fight(self, c1, c2):
+        round_count = 1
+        while c1.health > 0 and c2.health > 0:
+            c1.health -= random.randint(1, c2.attack)
+            c2.health -= random.randint(1, c1.attack)
+            print(f"ROUND: {round_count}", c1, c2)
+            round_count += 1
+        print("FIGHT OVER")
+        if c1.health <= 0:
+            if type(c1) is Monster:
+                self.monsters.remove(c1)
+            else:
+                print("PLAYER HAS DIED. GAME OVER.")
+                self.playing = False
+        if c2.health <= 0:
+            if type(c2) is Monster:
+                self.monsters.remove(c2)
+            else:
+                print("PLAYER HAS DIED. GAME OVER.")
+                self.playing = False
 
 
 game = Game()
--- player.py
+++ player.py
@@ -1,7 +1,24 @@
-class Player:
-    def __init__(self, name, symbol="^", location=[0, 0]):
+class Character:
+    def __init__(self, name, symbol, location=[0, 0]):
         self.name = name
         self.symbol = symbol
         self.location = location
         self.old_location = location
 
+        self.health = 100
+        self.attack = 10
+
+    def __str__(self):
+        return f"{self.name} has {self.health}."
+
+
+class Player(Character):
+    def __init__(self, name, symbol="^"):
+        super().__init__(name, symbol)
+        self.attack = 20
+
+
+class Monster(Character):
+    def __init__(self, name, symbol="M"):
+        super().__init__(name, symbol)
+        self.attack = 5
