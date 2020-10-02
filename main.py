4d64d139db54dd078addae0b711ca028424a441a
kevinelong <kevinelong@gmail.com>
2020-10-02 10:23:57 -0700

    add directions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from board import Board
 from player import Player
 
@@ -8,13 +7,13 @@ class Game:
         self.board = Board(9)
         self.player = Player("A", "^", self.board.player_location)
         self.playing = True
-        self.loop()
 
     def loop(self):
         while self.playing:
             self.update_display()
             self.user_input()
             self.apply_rules()
+        print("Thanks for playing!")
 
     def update_display(self):
         self.board.refresh()
@@ -22,8 +21,16 @@ class Game:
     def user_input(self):
         text = input("Which direction? or q to quit.")
         upper = text.upper()
-        if upper in ["NORTH", "UP", "W"]:
+        if upper == "Q":
+            self.playing = False
+        elif upper in ["NORTH", "UP", "W"]:
             self.move_player_up()
+        elif upper in ["SOUTH", "DOWN", "S"]:
+            self.move_player_down()
+        elif upper in ["WEST", "LEFT", "A"]:
+            self.move_player_left()
+        elif upper in ["EAST", "RIGHT", "D"]:
+            self.move_player_right()
 
     def move_player_up(self):
         x = self.player.location[0]
@@ -34,7 +41,36 @@ class Game:
         self.player.location[0] = x
         self.player.location[1] = y
 
+    def move_player_down(self):
+        x = self.player.location[0]
+        y = self.player.location[1]
+        y = y + 1
+        self.board.clear()
+        self.board.place(x, y, self.player.symbol)
+        self.player.location[0] = x
+        self.player.location[1] = y
+
+    def move_player_left(self):
+        x = self.player.location[0]
+        y = self.player.location[1]
+        x = x - 1
+        self.board.clear()
+        self.board.place(x, y, self.player.symbol)
+        self.player.location[0] = x
+        self.player.location[1] = y
+
+    def move_player_right(self):
+        x = self.player.location[0]
+        y = self.player.location[1]
+        x = x + 1
+        self.board.clear()
+        self.board.place(x, y, self.player.symbol)
+        self.player.location[0] = x
+        self.player.location[1] = y
+
     def apply_rules(self):
         pass
 
+
 game = Game()
+game.loop()
